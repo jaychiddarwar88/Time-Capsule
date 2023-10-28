86357ad86a1756aaf83facb0dc9d2273e16f91c9
Jay Chiddarwar <jschiddarwar@gmail.com>
2023-10-28 03:47:24 -0700

    Initial Commit

---
+++ Time Capsule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Time Capsule
+//
+//  Created by Jay Chiddarwar on 10/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Time Capsule/Time_CapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_CapsuleApp.swift
+//  Time Capsule
+//
+//  Created by Jay Chiddarwar on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Time_CapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
